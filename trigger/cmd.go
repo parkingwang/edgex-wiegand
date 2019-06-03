9da1c85275a647878b51e089dbb1d6f25ca2291e
yoojia.chen <yoojiachen@gmail.com>
2019-06-03 17:39:37 +0800

    Fixes dongkong bytes order

--- trigger/cmd.go
+++ trigger/cmd.go
@@ -17,19 +17,19 @@ func cmdToJSON(cmd *dongk.Command) (frames []byte, card string, doorId, direct b
 	// 1. 刷卡消息
 	// 2. 门磁、按钮、设备启动、远程开门记录
 	// 3. 报警记录
-	// Reader一个按顺序读取字节数组的包装类
-	reader := bytes.WrapReader(cmd.Data[:], dongk.ByteOrder)
 	json := jsonx.NewFatJSON()
 	json.Field("sn", cmd.SerialNum)
-	card = hex.EncodeToString(reader.NextBytes(4))
-	json.Field("card", card)
-	reader.NextBytes(7) // 丢弃timestamp数据
+	// Reader一个按顺序读取字节数组的包装类
+	reader := bytes.WrapReader(cmd.Data[:], dongk.ByteOrder)
 	json.Field("index", reader.NextUint32())
 	json.Field("type", reader.NextByte())
 	json.Field("state", reader.NextByte())
-	doorNo := reader.NextByte()
-	ioDirect := reader.NextByte()
-	json.Field("doorId", doorNo)
-	json.Field("direct", dongk.DirectName(ioDirect))
-	return json.Bytes(), card, doorNo, ioDirect
+	doorId = reader.NextByte()
+	direct = reader.NextByte()
+	card = hex.EncodeToString(reader.NextBytes(4))
+	json.Field("card", card)
+	reader.NextBytes(7) // 丢弃timestamp数据
+	json.Field("doorId", doorId)
+	json.Field("direct", dongk.DirectName(direct))
+	return json.Bytes(), card, doorId, direct
 }
